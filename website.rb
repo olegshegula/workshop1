7c5fad9233a50907fb47cd11bca0ca256686482d
oleg <oleg.shegula@gmail.com>
2014-12-08 13:54:45 +0200

    refactoring

--- website.rb
+++ website.rb
@@ -1,45 +1,33 @@
 require 'rubygems'
-#require 'sinatra'
 require 'bundler/setup'
 Bundler.require(:default)
 
-get '/' do
-  # @data =params[:data]
-
-  haml :index
-end
-
 post '/result' do
-
-  @data =params[:data]
-  @new_array= check_capitalize
-  @my_array=editTelephons
+  @data = params[:data]
+  @new_array = check_capitalize
+  @my_array = edit_phones
   haml :result
-
 end
 
-def editTelephons
-
-  my_array=[]
+def edit_phones
+  my_array = []
   my_line = @data.lines
   my_line.each do |line|
-    new_content = line.sub('000','093').sub('+38-','')
-    # puts new_content
-   my_array<<new_content
+    new_content = line.sub('000', '093').sub('+38-', '')
+    my_array << new_content
   end
-  my_array.sort_by { |line| line.split[1].capitalize  }.each do |i|
-  puts i
-
+  my_array.sort_by { |line| line.split[1].capitalize }.each do |i|
+    puts i
   end
   my_array
 end
 
 def check_capitalize
-  new_array=[]
+  new_array = []
   my_line = @data.lines
   my_line.each do |line|
     new_line = line.split
-   new_array<<new_line.each { |e| puts "The name #{e} is not capitalize! " if (e == e.capitalize).equal?(false) }
+    new_array << new_line.each { |e| puts "The name #{e} is not capitalize! " unless e == e.capitalize }
   end
   new_array
 end
